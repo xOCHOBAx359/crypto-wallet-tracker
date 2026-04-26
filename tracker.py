027ae07a54d30343ecdd72276ce2c270d6173705
xOCHOBAx359 <32067314+xOCHOBAx359@users.noreply.github.com>
2026-04-26 16:00:59 +0700

    Update tracker.py
    Added config

--- tracker.py
+++ tracker.py
@@ -1,15 +1,25 @@
 import requests
+from config import config
 
 def get_eth_balance(address):
-    url = f"https://api.blockcypher.com/v1/eth/main/addrs/{address}/balance"
-    response = requests.get(url)
+    url = f"{config.BLOCKCYPHER_BASE_URL}/addrs/{address}/balance"
     
-    if response.status_code == 200:
-        data = response.json()
-        balance = data["balance"] / 10**18
-        print(f"Balance: {balance} ETH")
-    else:
-        print("Error fetching balance")
+    try:
+        response = requests.get(url, timeout=config.REQUEST_TIMEOUT)
+        
+        if response.status_code == 200:
+            data = response.json()
+            balance = data["balance"] / 10**18
+            print(f"Balance: {balance} ETH")
+        else:
+            print("Error fetching balance")
+
+    except Exception as e:
+        if config.DEBUG:
+            print(f"Error: {e}")
+        else:
+            print("Request failed")
+
 
 if __name__ == "__main__":
     wallet = input("Enter ETH wallet address: ")
