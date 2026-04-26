ef5164992e4d4b13720315d1a5553464b9f13f82
xOCHOBAx359 <32067314+xOCHOBAx359@users.noreply.github.com>
2026-04-26 16:04:09 +0700

    Update tracker.py
    Upd utils

--- tracker.py
+++ tracker.py
@@ -1,16 +1,22 @@
 import requests
 from config import config
+from utils import is_valid_eth_address, wei_to_eth, format_balance
+
 
 def get_eth_balance(address):
+    if not is_valid_eth_address(address):
+        print("Invalid ETH address")
+        return
+
     url = f"{config.BLOCKCYPHER_BASE_URL}/addrs/{address}/balance"
-    
+
     try:
         response = requests.get(url, timeout=config.REQUEST_TIMEOUT)
-        
+
         if response.status_code == 200:
             data = response.json()
-            balance = data["balance"] / 10**18
-            print(f"Balance: {balance} ETH")
+            balance = wei_to_eth(data["balance"])
+            print(f"Balance: {format_balance(balance)}")
         else:
             print("Error fetching balance")
 
